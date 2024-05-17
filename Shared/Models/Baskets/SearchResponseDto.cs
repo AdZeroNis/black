7591f448c220756f7a8699c82fbc0636ab8857c6
AdZeroNis <shghyghkrimi2923@gmail.com>
2024-05-17 11:53:31 +0330

    add SearchResponseDto for basket

---
+++ Shared/Models/Baskets/SearchResponseDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models.Baskets
+{
+    public class SearchResponseDto
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public int Count { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string ProductImageFileName { get; set; }
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string UserLastName { get; set; }
+    }
+}
