e12107b2088dc2b1163cdff3d1b6cc60c8140bf5
AdZeroNis <shghyghkrimi2923@gmail.com>
2024-05-17 11:50:58 +0330

    add SearchRequestDto for basket

---
+++ Shared/Models/Baskets/SearchRequestDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models.Baskets
+{
+    public class SearchRequestDto
+    {
+        public int PageSize { get; set; }
+        public int PageNo { get; set; }
+        public int? Count { get; set; }
+        public DateTime? FromDate { get; set; }
+        public DateTime? ToDate { get; set; }
+        public string? UserName { get; set; }
+        public string? ProductName { get; set; }
+    }
+}
